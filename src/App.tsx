ee70e5536eb36aff8d79d4d195f5f1fdc2e98743
EvertonCandelone <everton.lcs.pro@gmail.com>
2023-05-31 18:05:42 -0300

    reduces

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import Header from './components/Header'
+import Produtos from './containers/Produtos'
+
+import { GlobalStyle } from './styles'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
+export type Produto = {
+  id: number
+  nome: string
+  preco: number
+  imagem: string
+}
+
+function App() {
+  const [favoritos, setFavoritos] = useState<Produto[]>([])
+
+  useEffect(() => {
+    fetch('/api/ebac_sports')
+      .then((res) => res.json())
+      .then((res) => Produtos(res))
+  }, [])
+
+  function favoritar(produto: Produto) {
+    if (favoritos.find((p) => p.id === produto.id)) {
+      const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id)
+      setFavoritos(favoritosSemProduto)
+    } else {
+      setFavoritos([...favoritos, produto])
+    }
+  }
+
+  return (
+    <Provider store={store}>
+      <GlobalStyle />
+      <div className="container">
+        <Header />
+        <Produtos favoritos={favoritos} favoritar={favoritar} />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
